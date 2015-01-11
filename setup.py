88947e924907fb5f111538ffce740aa0c4292bcc
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-01-12 03:06:10 +0530

    Setup things for PyPi release

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+        name='Universal',
+        version='1.9.2',
+        packages=['universal'],
+        license='GPLv3+',
+        long_description=open('README.md').read(),
+)
+
---
+++ universal/__init__.py
@@ -0,0 +1,12 @@
+"""
+Universal Competitive Programming Suite
+
+@author Shubham Chaudhary
+@email me@shubhamchaudhary.in
+"""
+
+#__title__       =   'Universal'
+#__author__      =   'Shubham Chaudhary'
+#__license__     =   'GPLv3+'
+#__copyright__   =   'Copyright 2014-2015 Shubham Chaudhary'
+#__version__     =   '1.9.1
