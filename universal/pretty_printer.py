8db267604768059a4246e48e6d64cfbbef8cd9de
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-05-10 00:59:45 +0530

    Removed help function with colored output
    If needed it can be extracted by reverting this commit

--- universal/pretty_printer.py
+++ universal/pretty_printer.py
@@ -11,36 +11,3 @@ MAGENTA = '\033[35m'
 CYAN = '\033[36m'
 WHITE = '\033[37m'
 RESET = '\033[39m'
-
-def helpFun():
-    print("")  # newline
-    # print( "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
-    print("    #######################################################")
-    print("    #        + + + ", Fore.YELLOW, "Universal Compiler Help", \
-          Fore.RESET, " + + +      (c) #", sep='')
-    print("    #                                                     #")
-    print("    # Aliases: '", GREEN, "universal", RESET, "' and '", GREEN, "u", \
-          RESET, "' and '", GREEN, "c", RESET, "'                #", sep='')
-    print("    # That means you may also use:                        #")
-    print("    #       `u --help`   or   `universal --help`          #")
-    print("    #                                                     #")
-    print("    # USAGE:  universal <filename>                        #")
-    print("    #         universal <filename> <test option>          #")
-    print("    # e.g      'universal hello.cpp'                      #")
-    print("    #          'universal HelloWorld.java'                #")
-    print("    # Automated Testing options: t, t1, t2, t3            #")
-    print("    # For this full help:  'universal -h'                 #")
-    print("    #                                                     #")
-    # print( "    # Supports with '.c' '.cpp' '.py' '.java' '.pl' '.sh' #")
-    print("    # File Extensions: ", BLUE, "*.c .cpp .py .java .pl .sh", \
-          RESET, "         #", sep='')
-    print("    #                                                     #")
-    print("    # ", RED, "Update Version", RESET, ": `", MAGENTA, "universal -u", \
-          RESET, "` i.e. `", MAGENTA, "u -u", RESET, "`          #", sep='')
-    print("    #              Or see README.md to get download link  #")
-    print("    #                                                     #")
-    print("    #######################################################")
-    print("    # Program: Universal Competitive Programming Suite    #")
-    print("    #######################################################")
-    # print( "# # # # # # # # # # # # # # # # # # # # # # # # # # # #"
-    print("")  # newline
