7d01790cabb401e247dcc2ee0777a0ca07fdc175
Shubham Chaudhary <me@shubhamchaudhary.in>
2015-05-10 18:58:21 +0530

    Make functions from unnecessary class methods

--- universal/compiler/language/gcc.py
+++ universal/compiler/language/gcc.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 import os
 from .language import Language
 from universal.config import EXECUTABLE_GCC, GCC_FLAGS
-from universal.pretty_printer import YELLOW, RESET
 from universal.util import perform_system_command, get_file_tuple
 
 
@@ -20,28 +19,28 @@ class Gcc(Language):
         :return: 0 if compilation successful
         """
         # Filename should be checked where called from
-        command, directory, name = self.get_command_for_compilation(self.filename)
-        out = perform_system_command(command)
-        return out
+        command, directory, name = get_command_for_compilation(self.filename)
+        return perform_system_command(command)
 
     def run(self):
-        command_run = self.get_command_to_run()
+        command_run = get_command_to_run(self.filename)
         return perform_system_command(command_run)
 
-    def get_command_for_compilation(self, filename):
-        (directory, name, extension) = get_file_tuple(filename)
-        output_filename = directory + '/' + name + '.out'
-        command = EXECUTABLE_GCC + " " + \
-                  GCC_FLAGS + \
-                  " -o " + output_filename + \
-                  ' ' + filename
-        return command, directory, name
 
-    def get_command_to_run(self):
-        (directory, name, extension) = get_file_tuple(self.filename)
-        output_file = directory + "/" + name + ".out"
-        command_run = output_file
-        test_file = directory + "/" + name + ".input"
-        if os.path.exists(test_file):
-            command_run += " < " + test_file
-        return command_run
+def get_command_to_run(filename):
+    (directory, name, extension) = get_file_tuple(filename)
+    output_file = directory + "/" + name + ".out"
+    command_run = output_file
+    test_file = directory + "/" + name + ".input"
+    if os.path.exists(test_file):
+        command_run += " < " + test_file
+    return command_run
+
+def get_command_for_compilation(filename):
+    (directory, name, extension) = get_file_tuple(filename)
+    output_filename = directory + '/' + name + '.out'
+    command = EXECUTABLE_GCC + " " + \
+              GCC_FLAGS + \
+              " -o " + output_filename + \
+              ' ' + filename
+    return command, directory, name
